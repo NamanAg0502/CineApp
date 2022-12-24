9b1af3216437feee3b7fcf1211c6b1f6d6a93000
Naman Agrawal <naman@192.168.1.3>
2022-12-24 22:29:59 +0530

    file structure completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Navbar, Profile, Movies, MoviesInfo, Actors } from './';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/movies/:id" element={<MoviesInfo />} />
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/App';
+import './index.css';
+
+import { ThemeProvider } from '@material-tailwind/react';
+import { BrowserRouter, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <BrowserRouter router={router}>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
